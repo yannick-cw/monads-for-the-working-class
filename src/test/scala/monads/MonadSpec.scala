442886471c8209bf0bf46ff1535b558c6f25a224
437580 <yannick.gladow@holidaycheck.com>
2017-07-04 17:13:19 +0200

    Adds MonadTransformer

--- src/test/scala/monads/MonadSpec.scala
+++ src/test/scala/monads/MonadSpec.scala
@@ -1,17 +1,19 @@
 package monads
 
 import monads.YourFunctors.Functor
-import org.scalatest.{AsyncWordSpecLike, Matchers, WordSpecLike}
+import monads.YourMonads.Monad
+import org.scalatest.{AsyncWordSpecLike, Matchers}
 
-import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.global
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 class MonadSpec extends AsyncWordSpecLike with Matchers {
 
   import YourMonads.MonadOps
 
   "You" should {
+    // easy
     "create a Monad for Option" ignore {
       import YourMonads.optionMonad
 
@@ -27,6 +29,7 @@ class MonadSpec extends AsyncWordSpecLike with Matchers {
       noneOption.yourFlatMap(stringToInt) shouldBe None
     }
 
+    // easy
     "create a Monad for Lists - wow how creative" ignore {
       import YourMonads.listMonad
 
@@ -38,11 +41,12 @@ class MonadSpec extends AsyncWordSpecLike with Matchers {
       List.empty[String].yourFlatMap(duplicate) shouldBe List.empty[String]
     }
 
+    // medium
     "create a Monad for Future - now it gets difficult" ignore {
       import YourMonads.futureMonad
 
-      def getName(userId: Int): Future[String]  = Future.successful("Bob")
-      def getAge(name: String): Future[Int]     = Future.successful(22)
+      def getName(userId: Int): Future[String] = Future.successful("Bob")
+      def getAge(name: String): Future[Int]    = Future.successful(22)
 
       getName(2)
         .yourFlatMap(name =>
@@ -51,6 +55,26 @@ class MonadSpec extends AsyncWordSpecLike with Matchers {
             age shouldBe 22
         })
     }
+
+    // advanced
+    "create a FutureOption Transformer - wow two monads create a new monad" ignore {
+      import YourMonads.optionMonad
+      import YourMonads.futureMonad
+      import Transformer.FutureOption
+
+      def thisCouldBeARealDb(id: Int): FutureOption[String] =
+        FutureOption(Future.successful(Some("User")))
+
+      def thisAsWell(name: String): FutureOption[Int] =
+        FutureOption(Future.successful(Some(22)))
+
+      val userAge: FutureOption[(String, Int)] = for {
+        user <- thisCouldBeARealDb(2)
+        age  <- thisAsWell(user)
+      } yield (user, age)
+
+      userAge.value.map(userAge => userAge shouldBe Some("User", 22))
+    }
   }
 }
 
@@ -58,6 +82,7 @@ object YourMonads {
 
   trait Monad[M[_]] extends Functor[M] {
     def pure[A](fa: A): M[A]
+
     def flatMap[A, B](fa: M[A], f: A => M[B]): M[B]
 
     // todo: make you life easier, implement map here once and for all!
@@ -69,12 +94,22 @@ object YourMonads {
     def yourFlatMap[B](f: A => M[B]): M[B] = ev.flatMap(a, f)
   }
 
-  // todo: challenges yourself - please do not use the `flatMap` or `map` of Option
+  // todo: challenges yourself - please do not use the `flatMap` or `map` of from standard library
   implicit def optionMonad: Monad[Option] = ???
 
-  // todo: challenges yourself - please do not use the `flatMap` or `map` of List
   implicit def listMonad: Monad[List] = ???
 
+  // you might need Promise
   implicit def futureMonad(implicit ec: ExecutionContext): Monad[Future] = ???
+
+}
+object Transformer {
+
+  case class FutureOption[A](
+      value: Future[Option[A]])(implicit ec: ExecutionContext, optMon: Monad[Option], futMonad: Monad[Future]) {
+    def pure(fa: A): FutureOption[A] = ???
+    def flatMap[B](f: A => FutureOption[B]): FutureOption[B] = ???
+    def map[B](f: A => B): FutureOption[B] = ???
+  }
 
 }
